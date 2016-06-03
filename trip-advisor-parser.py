236aa5834819dab9cf27c7fa5bc697c0c384e384
Andrea Esuli <aesuli@users.noreply.github.com>
2016-06-03 15:56:06 +0200

    Update trip-advisor-parser.py

--- trip-advisor-parser.py
+++ trip-advisor-parser.py
@@ -23,6 +23,9 @@ import re
 import sys
 import codecs
 
+if sys.version_info[0] >= 3:
+    import html
+
 
 def get_review_filesnames(input_dir):
     for root, dirnames, filenames in os.walk(input_dir):
@@ -42,6 +45,8 @@ reviewtextre = re.compile(r'<div class="entry">(.*?)</div>', re.M | re.S)
 aspectre = re.compile(r'recommend-answer(.*?)</li>', re.M | re.S)
 aspectratingre = re.compile(r'sprite-rating_ss_fill rating_ss_fill ss([0-9])0')
 aspectnamere = re.compile(r'span>(.*)$', re.M | re.S)
+hotelnamere = re.compile(r'warLocName">(.*)?</div>')
+idre = re.compile(r'id="review_([0-9]+)"')
 
 
 def get_aspect_ratings(block):
@@ -50,30 +55,51 @@ def get_aspect_ratings(block):
         rating = aspectratingre.findall(aspectrating)[0]
         name = aspectnamere.findall(aspectrating)[0].strip()
         rates.append(':'.join([name, rating]))
-    return ';'.join(rates)
+    return u';'.join(rates)
 
 
 def main():
-    sys.stdout = codecs.getwriter('utf8')(sys.stdout.buffer)
+    # sys.stdout = codecs.getwriter('utf8')(sys.stdout.buffer)
     parser = argparse.ArgumentParser(
-            description='TripAdvisor Hotel parser')
+        description='TripAdvisor Hotel parser')
     parser.add_argument('-d', '--dir', help='Directory with the data for parsing', required=True)
     parser.add_argument('-o', '--outfile', help='Output file path for saving the reviews in csv format', required=True)
 
     args = parser.parse_args()
 
-    with open(args.outfile, 'w', encoding='utf8') as out:
+    reviews = set()
+
+    with codecs.open(args.outfile, 'w', encoding='utf8') as out:
         writer = csv.writer(out, lineterminator='\n')
         for filepath in get_review_filesnames(args.dir):
-            with open(filepath, mode='r', encoding='utf8') as file:
+            with codecs.open(filepath, mode='r', encoding='utf8') as file:
                 htmlpage = file.read()
             print(filepath)
+            hotelname = hotelnamere.findall(htmlpage)[0]
             for block in summaryre.findall(htmlpage):
-                overallrating = overallratingre.findall(block)[0]
+                id_ = idre.findall(block)[0]
+                if id_ in reviews:
+                    continue
+                reviews.add(id_)
                 reviewtext = reviewtextre.findall(block)[0]
+                overallrating = int(overallratingre.findall(block)[0])
+                if overallrating >= 4:
+                    binaryrating = 'positive'
+                else:
+                    binaryrating = 'negative'
                 reviewtext = cleanhtml(reviewtext).strip()
-                aspectsratings = get_aspect_ratings(block)
-                writer.writerow([filepath, reviewtext, overallrating, aspectsratings])
+                if sys.version_info[0] >= 3:
+                    try:
+                        reviewtext = html.unescape(reviewtext)
+                    except Exception:
+                        pass
+                    review = [id_, filepath, hotelname, reviewtext, overallrating, binaryrating]
+                else:
+                    review = [id_, filepath,
+                                    unicode.encode(hotelname, encoding='ascii', errors='ignore'),
+                                    unicode.encode(reviewtext, encoding='ascii', errors='ignore'), overallrating,
+                                    binaryrating]
+                writer.writerow(review)
 
 
 if __name__ == '__main__':
